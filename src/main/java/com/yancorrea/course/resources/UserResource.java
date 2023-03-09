8fb8b64de1c53f52cc711c583fb6bf7913746ce3
Yan Souza <yansouza009@gmail.com>
2023-03-09 10:09:03 -0300

    User insert

--- src/main/java/com/yancorrea/course/resources/UserResource.java
+++ src/main/java/com/yancorrea/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.yancorrea.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.yancorrea.course.entities.User;
 import com.yancorrea.course.services.UserService;
@@ -33,4 +37,10 @@ public class UserResource {
 		
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
