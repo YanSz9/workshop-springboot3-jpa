036e815592a968dbb01d5458437409d17496232e
Yan Souza <yansouza009@gmail.com>
2023-03-13 10:11:24 -0300

    Exception handling - update

--- src/main/java/com/yancorrea/course/services/UserService.java
+++ src/main/java/com/yancorrea/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.yancorrea.course.repositories.UserRepository;
 import com.yancorrea.course.services.exceptions.DatabaseException;
 import com.yancorrea.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -40,9 +42,14 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
